6ac15780065f6c67245cf4382e81eddba8f70223
YardaRum <166068579+YardaRum@users.noreply.github.com>
2024-07-31 03:10:41 +0300

    Initial commit

--- src/main/java/com/example/task13/Task13Application.java
+++ src/main/java/com/example/task13/Task13Application.java
@@ -3,7 +3,6 @@ package com.example.task13;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Scanner;
